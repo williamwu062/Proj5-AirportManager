ca2d28c07c3174de201cbdeaf71107fb5ef76eb0
williamwu062 <wwuhoo@gmail.com>
2019-11-18 14:01:50 -0500

    Added methods and fields to BoardingPass and Passenger class. Not finished.

--- src/BoardingPass.java
+++ src/BoardingPass.java
@@ -1,2 +1,22 @@
+/**
+ * BoardingPass class that contains information on the passenger and flight.
+ */
 public class BoardingPass {
+	private String firstName;
+	private String lastName;
+	private int age;
+
+	/**
+	 * Initializes fields
+	 * @param firstName the firstName of the passenger
+	 * @param lastName the lastName of the passenger
+	 * @param age the age of the passenger
+	 */
+	public BoardingPass(String firstName, String lastName, int age) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+	//TODO need to add flight and flight number
 }
--- src/Passenger.java
+++ src/Passenger.java
@@ -1,2 +1,19 @@
+/**
+ * The Passenger class defines a passenger in a plane.
+ */
 public class Passenger {
+	private String firstName;
+	private String lastName;
+	private int age;
+	private BoardingPass pass;
+
+	public Passenger(String firstName, String lastName, int age) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+	public void addBoardingPass() {
+
+	}
 }
