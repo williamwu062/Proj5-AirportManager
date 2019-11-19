36b3285d6d11dc910efcee42cf8e754e5b458c46
williamwu062 <wwuhoo@gmail.com>
2019-11-18 20:02:03 -0500

    Added methods to the ReservationClient and ReservationServer. Made ReservationClient implement Runnable again

--- src/ReservationClient.java
+++ src/ReservationClient.java
@@ -1,6 +1,23 @@
-public class ReservationClient implements Runnable{
+import java.net.Socket;
+
+/**
+ * Client class where the user can buy a ticket if one is still available.
+ */
+public class ReservationClient implements Runnable {
+
+	private Socket socket;
+
+	/**
+	 * Initializes fields.
+	 *
+	 * @param socket the accepted socket
+	 */
+	public ReservationClient(Socket socket) {
+		this.socket = socket;
+	}
+
 	@Override
 	public void run() {
-
+		//TODO book a ticket and add and subtract from fields in ReservationServer to find number of seats left
 	}
 }
--- src/ReservationServer.java
+++ src/ReservationServer.java
@@ -3,6 +3,15 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ReservationServer {
+
+	public static int swSeats = 100;
+	public static int dSeats = 100;
+	public static int aSeats = 100;
+
+	public static Passenger[] swPassengers;
+	public static Passenger[] dPassengers;
+	public static Passenger[] aPassengers;
+
 	public static void main(String[] args) {
 		try {
 			ServerSocket socket = new ServerSocket(6679);
@@ -10,12 +19,12 @@ public class ReservationServer {
 			while (true) {
 				Socket s = socket.accept();
 
-				Thread t = new Thread();
+				Thread t = new Thread(new ReservationClient(s));
+
+				t.start();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-
 	}
 }
