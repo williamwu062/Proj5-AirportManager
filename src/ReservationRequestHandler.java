65c6c4bcabcc30adcce8f36225ec606eb71ce582
williamwu062 <wwuhoo@gmail.com>
2019-11-23 16:33:46 -0500

    Added Step 3 In Reservation Request Handler and also changed the GUI to a cardLayout to handle switching JPanels within the same frame.

--- src/ReservationRequestHandler.java
+++ src/ReservationRequestHandler.java
@@ -12,118 +12,166 @@ import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
 
-public class ReservationRequestHandler implements Runnable{
-    /**
-     * The client socket of this request handler.
-     */
-    private Socket clientSocket;
+public class ReservationRequestHandler implements Runnable {
+	/**
+	 * The client socket of this request handler.
+	 */
+	private Socket clientSocket;
+	private JFrame frame;
+	private JPanel mainPanel;
+	private CardLayout layout;
 
-    public ReservationRequestHandler(Socket clientSocket) throws NullPointerException {
-        Objects.requireNonNull(clientSocket, "the specified client socket is null");
+	/**
+	 * Initializes fields.
+	 *
+	 * @param clientSocket the socket to be set
+	 * @throws NullPointerException if clientSocket is null
+	 */
+	public ReservationRequestHandler(Socket clientSocket) throws NullPointerException {
+		Objects.requireNonNull(clientSocket, "the specified client socket is null");
+		this.clientSocket = clientSocket;
+		frame = new JFrame("Purdue University Flight Reservation System");
+		layout = new CardLayout();
+		mainPanel = new JPanel(layout);
 
-        this.clientSocket = clientSocket;
-    } //CensoringRequestHandler
+		stage_0();
 
-    /**
-     * Handles the requests of the client connected to this request handler's client socket.
-     */
-    public void stage2() {
-        JFrame stage2 = new JFrame("Purdue Airline Reservation System");
-        JPanel panelStage2 = new JPanel();
-        JLabel welcome = new JLabel("Welcome to Purdue University Airline Reservation Management System!");
-        JLabel imageWelcome = new JLabel(new ImageIcon("Images/Purdue_Boilermakers_logo.svg.png"));
-        JButton exit = new JButton("Exit");
-        exit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                stage8();
-            }
-        });
-        JButton BookAFlight = new JButton("Book a Flight");
-        BookAFlight.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                stage3();
-            }
-        });
-        stage2.setSize(900,600);
-        stage2.setLayout(new FlowLayout());
-        panelStage2.add(welcome);
-        panelStage2.add(imageWelcome, BorderLayout.CENTER);
-        stage2.add(panelStage2);
-        stage2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        stage2.setVisible(true);
-    }
+		frame.add(mainPanel);
+		frame.setSize(900, 600);
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
+	} //CensoringRequestHandler
 
-    public void stage3() {
+	/**
+	 * Handles the requests of the client connected to this request handler's client socket.
+	 */
+	public void stage_0() {
+		JPanel panel = new JPanel();
+		JLabel welcome = new JLabel("Welcome to Purdue University Airline Reservation Management System!");
+		panel.add(welcome);
+		JButton exit = new JButton("Exit");
+		panel.add(exit);
+		JButton next = new JButton("Book a Flight");
+		panel.add(next);
 
-    }
+		exit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				stage_7();
+			}
+		});
 
-    public void stage4() {
+		next.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				stage_1();
+			}
+		});
 
-    }
+		mainPanel.add(panel, "0");
+		layout.show(mainPanel, "0");
 
-    public void stage5() {
+		//JLabel imageWelcome = new JLabel(new ImageIcon("Images/Purdue_Boilermakers_logo.svg.png"));
+		//panelStage2.add(imageWelcome, BorderLayout.CENTER);
+	}
 
-    }
+	public void stage_1() {
+		JPanel panel = new JPanel();
+		JLabel welcome = new JLabel("Are You Sure You Want to Book a Flight?");
+		panel.add(welcome);
+		JButton exit = new JButton("No");
+		panel.add(exit);
+		JButton next = new JButton("Yes");
+		panel.add(next);
 
-    public void stage6() {
+		exit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				stage_7();
+			}
+		});
 
-    }
+		next.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				stage_2();
+			}
+		});
 
-    public void stage7() {
+		mainPanel.add(panel, "1");
+		layout.show(mainPanel, "1");
+	}
 
-    }
+	public void stage_2() {
 
-    public void stage8() {
+	}
 
-    }
-    @Override
-    public void run() {
-        stage2();
-    } //run
+	public void stage_3() {
 
-    /**
-     * Returns the hash code of this request handler.
-     *
-     * @return the hash code of this request handler
-     */
-    @Override
-    public int hashCode() {
-        int result = 23;
+	}
 
-        result = 31 * result + Objects.hashCode(this.clientSocket);
+	public void stage_4() {
 
-        return result;
-    } //hashCode
+	}
 
-    /**
-     * Determines whether or not the specified object is equal to this request handler. {@code true} is returned if and
-     * only if the specified object is an instance of {@code CensoringRequestHandler} and its client socket and
-     * {@code Set} of bad words are equal to this request handler's.
-     *
-     * @param object the object to be used in the comparisons
-     * @return {@code true}, if the specified object is equal to this request handler and {@code false} otherwise
-     */
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        } else if (object instanceof ReservationRequestHandler) {
-            boolean equal;
+	public void stage_5() {
 
-            equal = Objects.equals(this.clientSocket, ((ReservationRequestHandler) object).clientSocket);
+	}
 
-            return equal;
-        } else {
-            return false;
-        } //end if
-    } //equals
+	public void stage_6() {
 
-    @Override
-    public String toString() {
-        String format = "CensoringRequestHandler[%s]";
+	}
 
-        return String.format(format, this.clientSocket);
-    } //toString
+	public void stage_7() {
+
+	}
+
+	@Override
+	public void run() {
+
+	} //run
+
+	/**
+	 * Returns the hash code of this request handler.
+	 *
+	 * @return the hash code of this request handler
+	 */
+	@Override
+	public int hashCode() {
+		int result = 23;
+
+		result = 31 * result + Objects.hashCode(this.clientSocket);
+
+		return result;
+	} //hashCode
+
+	/**
+	 * Determines whether or not the specified object is equal to this request handler. {@code true} is returned if and
+	 * only if the specified object is an instance of {@code CensoringRequestHandler} and its client socket and
+	 * {@code Set} of bad words are equal to this request handler's.
+	 *
+	 * @param object the object to be used in the comparisons
+	 * @return {@code true}, if the specified object is equal to this request handler and {@code false} otherwise
+	 */
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		} else if (object instanceof ReservationRequestHandler) {
+			boolean equal;
+
+			equal = Objects.equals(this.clientSocket, ((ReservationRequestHandler) object).clientSocket);
+
+			return equal;
+		} else {
+			return false;
+		} //end if
+	} //equals
+
+	@Override
+	public String toString() {
+		String format = "CensoringRequestHandler[%s]";
+
+		return String.format(format, this.clientSocket);
+	} //toString
 }
