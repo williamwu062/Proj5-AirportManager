dfd814351cb147eaabe0ed3c5b8bd14f3f447739
Benjamin_Zhu <zhu862@purdue.edu>
2019-11-27 19:06:31 -0500

    Changed Passenger and requestHandler

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,3 +1,10 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * The Passenger class defines a passenger in a plane.
  */
@@ -6,14 +13,65 @@ public class Passenger {
 	private String lastName;
 	private int age;
 	private BoardingPass pass;
+	private ArrayList<String> names;
 
-	public Passenger(String firstName, String lastName, int age) {
+	public Passenger(String firstName, String lastName, int age, ArrayList<String> names) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.age = age;
+		this.names = names;
 	}
 
-	public void addBoardingPass() {
-		//TODO create a boarding pass
+	public ArrayList<String> getEachPassenger(String string) {
+		File file = new File("reservations.txt");
+		String exception1 = "";
+		String exception2= "";
+		if(string.equals("ALASKA")) {
+			exception1 = "DELTA";
+			exception2 = "SOUTHWEST";
+		}
+		else if (string.equals("DELTA")) {
+			exception1 = "ALASKA";
+			exception2 = "SOUTHWEST";
+		}
+		else {
+			exception1 = "ALASKA";
+			exception2 = "DELTA";
+		}
+		try {
+			FileReader fr = new FileReader(file);
+			BufferedReader bfr = new BufferedReader(fr);
+			String s ="";
+
+			while(true) {
+				if(!string.equals("")) {
+					s = bfr.readLine();
+					if(s == null || s.equals("EOF")) {
+						break;
+					}
+					if(s.equals(string)) {
+						if(!s.contains(".") || s.contains("/") || s.equals("")) {
+							bfr.readLine();
+						}
+						else {
+							this.names.add(s);
+						}
+
+						if(s.equals(exception1) || s.equals(exception2)) {
+							break;
+						}
+					}
+				}
+
+			}
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "The file does not exist!", "File not found", JOptionPane.ERROR_MESSAGE);
+		}
+		return this.names;
 	}
+
+	public void addPassengerToList() {
+
+	}
+
 }
